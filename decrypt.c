83e574d9068261b20c6ba85e5416a84afbc77089
GZHYBFHHJ <82015237+GZHYBFHHJ@users.noreply.github.com>
2022-11-12 22:01:38 +0800

    修复缩进

--- decrypt.c
+++ decrypt.c
@@ -60,33 +60,33 @@ int encrypt() {
 
 int decrypt() {
 	FILE *in =  fopen(ENC, "rb");
-		if (!in) {
-			printf("打开文件失败!\n");
-			perror("fopen");
-			return -1;
-		}
-		
-		FILE *out = fopen(DEC, "wb");
-		if (!out) {
-			printf("创建文件失败!\n");
-			perror("fopen");
-			fclose(in);
-			return -1;
-		}
-		
-		#define BUFFERSIZE 256
-		
-		uint32_t buffer[BUFFERSIZE];
-		int read;
-		while (read = fread(buffer, 4, BUFFERSIZE, in)) {
-			for (int i = 0; i < BUFFERSIZE; i++) {
-				buffer[i] ^= PASSWORD;
-			}
-			fwrite(buffer, 4, read, out);
-		}
-		
+	if (!in) {
+		printf("打开文件失败!\n");
+		perror("fopen");
+		return -1;
+	}
+	
+	FILE *out = fopen(DEC, "wb");
+	if (!out) {
+		printf("创建文件失败!\n");
+		perror("fopen");
 		fclose(in);
-		fclose(out);
+		return -1;
+	}
+	
+	#define BUFFERSIZE 256
 		
-		return 0;
+	uint32_t buffer[BUFFERSIZE];
+	int read;
+	while (read = fread(buffer, 4, BUFFERSIZE, in)) {
+		for (int i = 0; i < BUFFERSIZE; i++) {
+			buffer[i] ^= PASSWORD;
+		}
+		fwrite(buffer, 4, read, out);
+	}
+		
+	fclose(in);
+	fclose(out);
+		
+	return 0;
 }
